bcf2450cf478ef332a583d2de212959dfd48052c
PenUp <payteltech@paytelgroup.com>
2021-09-28 18:40:57 +0530

    Create common PopUpDialog (jay) Changes on PrivacyFragment(jay) Create chatBackup Fragment(jay)

---
+++ app/src/main/java/com/penup/fragment/ChatBackupFragment.java
@@ -0,0 +1,24 @@
+package com.penup.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.penup.R;
+
+
+public class ChatBackupFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_backup, container, false);
+    }
+}
