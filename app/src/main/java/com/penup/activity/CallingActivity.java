5aa80bfcc06a80ee2b0998b92c3c9da5fc846d29
Paytel Android <payteltech@paytelgroup.com>
2021-09-30 16:59:00 +0530

    audio & video Jitsi library integrated(sagar)

---
+++ app/src/main/java/com/penup/activity/CallingActivity.java
@@ -0,0 +1,16 @@
+package com.penup.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.penup.R;
+
+public class CallingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calling);
+    }
+}
